9e3f8b148c02c1d3161ed24535e4115af22870a6
Matt Cui <cuixuex@cn.ibm.com>
2022-05-14 11:01:41 +0800

    Test running gcloud to connect to gke cluster

---
+++ deploy/kubernetes/scripts/deploy_capact_gke.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Copyright 2022 Nordcloud Oy or its affiliates. All Rights Reserved.
+
+set -eE -o pipefail
+shopt -s extdebug
+
+WORKSPACE="$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)"
+
+readonly NAMESPACE='capact-system'
+readonly WORKSPACE
+
+echo "Install/Upgrade neo4j release"
+helm -n "${NAMESPACE}" upgrade --install --wait neo4j "${WORKSPACE}"/charts/neo4j
+
+echo "Install/Upgrade ingress-nginx release"
+helm -n "${NAMESPACE}" upgrade --install --wait ingress-nginx "${WORKSPACE}"/charts/ingress-controller
+
+echo "Install/Upgrade argo release"
+if ! helm -n "${NAMESPACE}" get notes argo > /dev/null 2>&1; then
+   helm -n "${NAMESPACE}" install argo "${WORKSPACE}"/charts/argo
+else
+   ACCESS_KEY=$(kubectl get secret --namespace "capact-system" argo-minio -o jsonpath="{.data.access-key}" | base64 --decode)
+   SECRET_KEY=$(kubectl get secret --namespace "capact-system" argo-minio -o jsonpath="{.data.secret-key}" | base64 --decode)
+   helm -n "${NAMESPACE}" upgrade --install --wait argo "${WORKSPACE}"/charts/argo --set minio.accessKey.password="${ACCESS_KEY}" --set minio.secretKey.password="${SECRET_KEY}"
+fi
+
+echo "Install/Upgrade cert-manager release"
+helm -n "${NAMESPACE}" upgrade --install --wait cert-manager "${WORKSPACE}"/charts/cert-manager
+
+echo "Install/Upgrade kubed release"
+helm -n "${NAMESPACE}" upgrade --install --wait kubed "${WORKSPACE}"/charts/kubed
+
+echo "Install/Upgrade capact release"
+helm -n "${NAMESPACE}" upgrade --install --wait capact "${WORKSPACE}"/charts/capact
+
+helm list -n "${NAMESPACE}"
